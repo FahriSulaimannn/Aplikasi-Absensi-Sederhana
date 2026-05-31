1626b502160ea9f613560b58941fbe9a60296981
Ahmad <ahmadasgf89@gmail.com>
2026-05-31 15:15:16 +0700

    chore: add sample ada divisi

---
+++ migration/data-divisi.sql
@@ -0,0 +1,5 @@
+INSERT INTO divisi (nama) VALUES ('IT');
+INSERT INTO divisi (nama) VALUES ('HRD');
+INSERT INTO divisi (nama) VALUES ('Finance');
+INSERT INTO divisi (nama) VALUES ('Marketing');
+INSERT INTO divisi (nama) VALUES ('Operations');
